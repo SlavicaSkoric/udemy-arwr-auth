7d26cfb3644f7cd6e0de20cda5d240950d2cf1d1
SlavicaSkoric <skoric.slavica.hr@gmail.com>
2022-03-05 01:09:17 +0100

    add bcrypt-nodejs and jwt-simple

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,4 +1,17 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+// first argument - information that we want to encode; object
+// sub & iat - conventions of JWT standard
+// sub - subject
+// iat - issued at time
+// JWT - standard
+// second argument - the secret that we're going to use to encrypt it
 
 exports.signup = function (req, res, next) {
   // res.send({ success: 'true' });
@@ -7,6 +20,12 @@ exports.signup = function (req, res, next) {
 
   const email = req.body.email;
   const password = req.body.password;
+
+  if (!email || !password) {
+    return res
+      .status(422)
+      .send({ error: 'You must provide email and password' });
+  }
 
   // see if a user with the given email exists
   User.findOne({ email: email }, function (err, existingUser) {
@@ -32,7 +51,8 @@ exports.signup = function (req, res, next) {
 
       // respond to request indicating the user was created
       // res.json(user);
-      res.json({ success: true });
+      // res.json({ success: true });
+      res.json({ token: tokenForUser(user) });
     });
   });
 };
--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,38 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 // define-create our model
 const userSchema = new Schema({
   email: { type: String, unique: true, lowercase: true },
   password: String,
+});
+
+// on save hook, encrypt password
+// before saving a model, run this function
+userSchema.pre('save', function (next) {
+  // get access to the user model
+  const user = this;
+  // user is an instance of the user model at this time
+  // user.email, user.password
+
+  // generate a salt then run callback
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) {
+      return next(err);
+    }
+
+    // hash (encrypt) our password using the salt
+    bcrypt.hash(user.password, salt, null, function (err, hash) {
+      if (err) {
+        return next(err);
+      }
+
+      // overwrite plain text password with encrypted password
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 // create the model class
